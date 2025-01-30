8b454e06537e13aacfc19f010280a46fce560951
subham singh <shubhsm.singh@lumiq.ai>
2025-01-30 23:07:42 +0530

    minor changes

--- src/main/java/org/example/Interface/Abstract/CommanData.java
+++ src/main/java/org/example/Interface/Abstract/CommanData.java
@@ -3,7 +3,7 @@ package org.example.Interface.Abstract;
 public abstract class CommanData {
 
     public void sum(){
-        System.out.println("we are in some ");
+        System.out.println("we  in some ");
     }
     public abstract void sum1();
 
